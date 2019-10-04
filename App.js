b41d49d91b8ca915f893579a804497cea99a1359
Areum Jo <areumjo1@gmail.com>
2019-10-03 19:20:55 -0700

    Added GameScreen compo and conditional rendering btw StartGame, Game

--- App.js
+++ App.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 
 import Header from './components/Header';
 import StartGameScreen from './screens/StartGameScreen';
+import GameScreen from './screens/GameScreen';
 
 
 export default function App() {
+  // for conditional rendering
+  // userNumber is getting from StartGameScreen compo and send it to GameScreen
+  const [userNumber, setUserNumber] = useState();
+
+  const startGameHandler = (selectedNumber) => {
+    setUserNumber(selectedNumber);
+  };
+
+  let content = <StartGameScreen onStartGame={startGameHandler}/>
+  if (userNumber) {
+    content = <GameScreen userChoice={userNumber} />
+  }
+
   return (
     <View style={styles.screen}>
       <Header title="Guess a Number" />
-      <StartGameScreen />
+      {content}
     </View>
   );
 }
