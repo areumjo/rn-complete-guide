dc24aeca132a5e98514deed45ce8f36471866eeb
Areum Jo <areumjo1@gmail.com>
2019-10-03 17:15:33 -0700

    Added Reset/Confirm functionality and confirmed output on screen

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -8,8 +8,32 @@ import Colors from "../constants/colors";
 const StartGameScreen = props => {
   // Validation for number with regEx
   const [enteredValue, setEnteredValue] = useState('');
+  const [confirmed, setConfirmed] = useState(false);
+  const [selectedNumber, setSelectedNumber] = useState();
+
   const numberInputHandler = inputText => {
     setEnteredValue(inputText.replace(/[^0-9]/g), '');
+  }
+
+  const resetInputHandler = () => {
+    setEnteredValue('');
+    setConfirmed(false);
+  }
+
+  const confirmInputHandler = () => {
+    const chosenNumber = parseInt(enteredValue);
+    if (chosenNumber === NaN || chosenNumber <= 0 || chosenNumber > 99) {
+        return;
+    }
+    setConfirmed(true);
+    setSelectedNumber(chosenNumber);
+    setEnteredValue('');
+  }
+
+  let confirmedOutput;
+
+  if (confirmed) {
+    confirmedOutput = <Text>Chosen Number: {selectedNumber}</Text>
   }
 
   return (
@@ -32,12 +56,13 @@ const StartGameScreen = props => {
                         value={enteredValue}
                     />
                     <View style={styles.buttonContainer}>
-                        <View style={styles.button}><Button title="Reset" onPress={() => {}} color={Colors.accent} /></View>
-                        <View style={styles.button}><Button title="Confirm" onPress={() => {}} color={Colors.primary}
+                        <View style={styles.button}><Button title="Reset" onPress={resetInputHandler} color={Colors.accent} /></View>
+                        <View style={styles.button}><Button title="Confirm" onPress={confirmInputHandler} color={Colors.primary}
                         /></View>
                     </View>
                 </View>
             </Card>
+            {confirmedOutput}
         </View>
       </TouchableWithoutFeedback>
   );
